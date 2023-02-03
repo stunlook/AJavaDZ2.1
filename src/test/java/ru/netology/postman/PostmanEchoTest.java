9d73aab5de14f76e3ef9331e83943ac8de2d08e8
Stepan Smirnov <stunlooksteff@gmail.com>
2023-02-03 21:04:12 +0300

    initial Code

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.postman;
+
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.is("some data" ))
+        ;
+    }
+
+}
